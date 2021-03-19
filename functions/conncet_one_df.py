7a1277af01b1e7a19f4f686d8030bd8f6736a01d
Marek <marek.brynda96@gmail.com>
2021-03-19 21:03:50 +0100

    creating map of colors

--- functions/conncet_one_df.py
+++ functions/conncet_one_df.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 
-def connect_one_df(year):
-	folder = 'wyniki_wyborow/' + year + '/'
+def connect_one_df(year,level):
+	folder = '../wyniki_wyborow/' + year + '/'
 
 	dfTOTAL = None
 
@@ -24,25 +24,26 @@ def connect_one_df(year):
 			folder_1 = folder+o+'/'
 			powiaty = os.listdir(folder_1)
 			#dfTOTAL = pd.concat([dfTOTAL, add_name_of_district(fol =folder_1, ok = powiaty ,d = 'O_')])
-			#dfTOTAL = pd.concat([dfTOTAL, add_name_of_district(fol =folder_1, ok = powiaty ,d = '')])
-			
-			for p in powiaty:
-				if os.path.isdir(folder_1+p):
-					folder_2 = folder_1+p+'/'
-					gminy = os.listdir(folder_2)
-					dfTOTAL = pd.concat([dfTOTAL, add_name_of_district(fol =folder_2, ok = gminy ,d = 'P_')])		
-					'''
-					for g in gminy:
-						if os.path.isdir(folder_2+g):
-							folder_3 = folder_2+g+'/'
-							gminy = os.listdir(folder_3)
-							dfTOTAL = pd.concat([dfTOTAL, add_name_of_district(fol =folder_3, ok = gminy ,d = 'G_')])
-					'''
+			if('w'==level):
+				dfTOTAL = pd.concat([dfTOTAL, add_name_of_district(fol =folder_1, ok = powiaty ,d = '')])
+			else:
+				for p in powiaty:
+					if os.path.isdir(folder_1+p):
+						folder_2 = folder_1+p+'/'
+						gminy = os.listdir(folder_2)
+						
+						if('p'==level):
+							dfTOTAL = pd.concat([dfTOTAL, add_name_of_district(fol =folder_2, ok = gminy ,d = '')])		
+						else:
+							for g in gminy:
+								if os.path.isdir(folder_2+g):
+									folder_3 = folder_2+g+'/'
+									gminy = os.listdir(folder_3)
+									dfTOTAL = pd.concat([dfTOTAL, add_name_of_district(fol =folder_3, ok = gminy ,d = '')])
 
-	dfTOTAL.to_csv(year+'_W.csv', encoding='utf-8')
+	dfTOTAL.to_csv(year+'_'+level+'.csv', encoding='utf-8')
 
-years = ['2005', '2007', '2011']
-years = ['2001']
+years = ['2001','2005', '2007', '2011']
 
 for y in years:
-	connect_one_df(y)
+	connect_one_df(y, 'p')
